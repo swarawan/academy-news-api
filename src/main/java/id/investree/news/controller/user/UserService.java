7de87ab817fb7bb8e8e20973763d72b9e69b734b
Rio Swarawan <swarawan.rio@gmail.com>
2019-03-14 14:36:02 +0700

    [RIO] controller modification

--- src/main/java/id/investree/news/controller/user/UserService.java
+++ src/main/java/id/investree/news/controller/user/UserService.java
@@ -5,9 +5,9 @@ import id.investree.news.model.request.UserRequest;
 
 public interface UserService {
 
-    User save(UserRequest request);
+    User save(UserRequest request) throws Exception;
 
-    User update(Long id, UserRequest request);
+    User update(Long id, UserRequest request) throws Exception;
 
     User findBy(Long id);
 
--- src/main/java/id/investree/news/security/HmacAlgorithm.java
+++ src/main/java/id/investree/news/security/HmacAlgorithm.java
@@ -1,12 +1,20 @@
 package id.investree.news.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.util.Formatter;
 
+@Component
 public class HmacAlgorithm {
+
+    @Value("$key")
+    private String key;
 
     private String toHexString(byte[] bytes) {
         String result = "";
@@ -20,8 +28,11 @@ public class HmacAlgorithm {
         }
     }
 
-    public String generate(String plain, String key, String alg)
-            throws NoSuchAlgorithmException, InvalidKeyException {
+    public String generate(String plain) throws InvalidKeyException, NoSuchAlgorithmException {
+        return generate(plain, key, "HmacSHA256");
+    }
+
+    public String generate(String plain, String key, String alg) throws NoSuchAlgorithmException, InvalidKeyException {
         SecretKeySpec signingKey = new SecretKeySpec(key.getBytes(), alg);
         Mac mac = Mac.getInstance(alg);
         mac.init(signingKey);
